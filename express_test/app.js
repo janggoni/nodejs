b79ab6375ddc88ce7143960fb6eac1fc02a71a15
janggoni <janggoni@klnet.co.kr>
2021-04-15 17:43:06 +0900

    doit -05-14

--- express_test/app.js
+++ express_test/app.js
@@ -7,4 +7,4 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 var server = http.createServer(app).listen(app.get('port'), function(){
     console.log('익스프레스 웹서버 실행 :' +app.get('port'));
-});
+}); 
